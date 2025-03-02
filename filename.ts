19f9562f832b22f123b37228464d82a8c384218e
Asad Ali <mo.asadali007@gmail.com>
2025-03-02 22:02:26 +0530

    learning typescript

--- filename.ts
+++ filename.ts
@@ -178,29 +178,169 @@
 // const merged = mergeObjects(obj1, obj2);
 // console.log(merged);
 
-const original = { 
-    name: "Asad", 
-    details: { age: 22, skills: ["TypeScript", "Next.js"] } 
-};
-function deepClone(obj: any): any {
-    if (typeof obj!== 'object' || obj === null) {
-        return obj;
-    }
-    const clone: any = Array.isArray(obj)? [] : {};
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            clone[key] = deepClone(obj[key]);
-        }
-    }
-    return clone;
-}
-const cloned = deepClone(original);
-cloned.details.age = 25;
-cloned.details.skills.push("React");
+// const original = { 
+//     name: "Asad", 
+//     details: { age: 22, skills: ["TypeScript", "Next.js"] } 
+// };
+// function deepClone(obj: any): any {
+//     if (typeof obj!== 'object' || obj === null) {
+//         return obj;
+//     }
+//     const clone: any = Array.isArray(obj)? [] : {};
+//     for (const key in obj) {
+//         if (obj.hasOwnProperty(key)) {
+//             clone[key] = deepClone(obj[key]);
+//         }
+//     }
+//     return clone;
+// }
+// const cloned = deepClone(original);
+// cloned.details.age = 25;
+// cloned.details.skills.push("React");
 
-console.log(original.details.age); // Should still be 22
-console.log(original.details.skills); // Should still be ["TypeScript", "Next.js"]
-console.log(cloned.details.age);// Should still be 25
-console.log(cloned.details.skills); // Output: ["TypeScript", "Next.js", "React"]
+// console.log(original.details.age); // Should still be 22
+// console.log(original.details.skills); // Should still be ["TypeScript", "Next.js"]
+// console.log(cloned.details.age);// Should still be 25
+// console.log(cloned.details.skills); // Output: ["TypeScript", "Next.js", "React"]
+
+// interface User {
+//     name?: string;
+//     id?: number;
+//     role?: string;
+//   }
+  
+//   class UserAccount {
+//     name: string;
+//     id: number;
+//     role: string;
+  
+//     constructor(name: string, id: number, role: string) {
+//       this.name = name;
+//       this.id = id;
+//       this.role = role;
+//     }
+//   }
+  
+//   const user1: User = new UserAccount("Murphy", 1, "admin");
+//   const user2: User = new UserAccount("Murphy", 1, "user");
 
 
+//   function deleteUser(user: User) {
+//     delete user.name;
+//     delete user.id;
+//     delete user.role;
+//     return user;
+//   }
+   
+//   function getAdminUser(user:User): User {
+//     if(user.role === "admin"){
+//         return user;
+//     }
+//     throw new Error("The user is not an admin.");
+//   }
+
+//   console.log(getAdminUser(user1)); // Output: UserAccount { name: 'Murphy', id: 1, role: 'admin' }
+//   console.log(deleteUser(user2)); // Output: {}
+
+
+// const jsonParserUnknown = (jsonString: string): unknown => JSON.parse(jsonString);
+
+// const myOtherAccount = jsonParserUnknown(`{ "name": "Samuel" }`);
+
+// console.log(myOtherAccount);  Output:{ name: 'Samuel' }
+
+// myUserAccount.name; // Error: Property 'name' does not exist on type 'unknown'.
+
+//type User = { name: string };
+// const myUserAccount = jsonParserUnknown(`{ "name": "Samuel" }`) as User;
+// console.log(myUserAccount.name); // Output: Samuel
+
+// const neverReturns = () => {
+//     // If it throws on the first line
+//     throw new Error("Always throws, never returns");
+//   };
+//   const myValue = neverReturns();
+//   console.log(myValue); // Output: Error: Always throws, never returns
+//   const validateUser = (user: User) => {
+//     if (user) {
+//       return user.name !== "NaN";
+//     }
+//     return neverReturns();
+// };
+
+// // const myUser = { name: "Samuel" };
+// const myUser={name: "NaN"};
+
+// const isValid = validateUser(myUser);
+// console.log(isValid); // Output: true
+
+// type NeverIsRemoved = string | never | number;
+
+// enum Flower {
+//     Rose,
+//     Rhododendron,
+//     Violet,
+//     Daisy,
+//   }
+  
+//   const flowerLatinName = (flower: Flower) => {
+//     switch (flower) {
+//       case Flower.Rose:
+//         return "Rosa rubiginosa";
+//       case Flower.Rhododendron:
+//         return "Rhododendron ferrugineum";
+//       case Flower.Violet:
+//         return "Viola reichenbachiana";
+//       case Flower.Daisy:
+//         return "Bellis perennis";
+  
+//       default:
+//         const _exhaustiveCheck: never = flower;
+//         return _exhaustiveCheck;
+//     }
+//   };
+  
+//   console.log(flowerLatinName(Flower.Rose)); // Output: Rosa rubiginosa
+//   console.log(flowerLatinName(Flower.Rhododendron)); // Output: Rhododendron ferrugineum
+//   console.log(flowerLatinName(Flower.Violet)); // Output: Viola reichenbachiana
+//   console.log(flowerLatinName(Flower.Daisy)); // Output: Bellis perennis
+//   console.log(flowerLatinName(5)); // Output: Error: Argument of type '5' is not assignable to parameter of type 'Flower'.
+
+// type StringArray = Array<string>;
+// type NumberArray = Array<number>;
+// type ObjectWithNameArray = Array<{ name: string }>;
+
+// interface Point {
+//     x: number;
+//     y: number;
+//   }
+   
+//   function logPoint(p: Point) {
+//     console.log(`${p.x}, ${p.y}`);
+//   }
+   
+//   // logs "12, 26"
+//   const point = { x: 12, y: 26 };
+//   logPoint(point);
+
+//   const point3 = { x: 12, y: 26, z: 89 };
+// logPoint(point3); // logs "12, 26"
+ 
+// const rect = { x: 33, y: 3, width: 30, height: 80 };
+// logPoint(rect); // logs "33, 3"
+ 
+// // const color = { hex: "#187ABF" };
+// // logPoint(color); // Argument of type '{ hex: string; }' is not assignable to parameter of type 'Point'.
+
+// class VirtualPoint {
+//     x: number;
+//     y: number;
+   
+//     constructor(x: number, y: number) {
+//       this.x = x;
+//       this.y = y;
+//     }
+//   }
+   
+//   const newVPoint = new VirtualPoint(13, 56);
+//   logPoint(newVPoint); // logs "13, 56"
